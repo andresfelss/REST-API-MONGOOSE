ccfdc463097bfa5a4e639b670011f42b20537beb
andre <andres.felipepr17@gmial.com>
2022-05-12 19:22:18 -0500

    Setting the route of edit succesfuly using method Overide also i install it

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const Product = require('./models/product');
 
@@ -8,6 +9,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
+app.use(methodOverride('_method'));
 
 // Para conectarnos con Mongo.
 main().catch(err => console.log(err))
@@ -31,6 +33,21 @@ app.post('/products', async (req,res) =>{
     await newProduct.save();
     res.redirect(`/products/${newProduct._id}`);
 })
+
+// Updating Product
+
+app.get('/products/:id/edit', async(req,res)=>{
+    const { id } = req.params;
+    const product = await Product.findById({_id: id});
+    res.render('products/edit', { product });
+});
+app.put('/products/:id', async (req,res) =>{
+    console.log(req.body);
+    res.send('PUTTT');
+})
+
+
+
 
 // Details products
 app.get('/products/:id', async(req,res)=>{
